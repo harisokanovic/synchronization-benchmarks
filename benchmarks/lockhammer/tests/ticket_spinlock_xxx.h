aa8b6f8a6677d8afe1e146f57331b5aaa226b92e
Haris Okanovic <harisokn@amazon.com>
2023-11-16 17:23:56 -0600

    add spin pause

--- benchmarks/lockhammer/tests/ticket_spinlock_xxx.h
+++ benchmarks/lockhammer/tests/ticket_spinlock_xxx.h
@@ -1,13 +1,27 @@
 // XXX
 
+static inline void spin_pause() {
+  #if defined(__x86_64__)
+    asm volatile("pause;");
+  #elif defined(__aarch64__)
+    asm volatile("yield;");
+  #else
+    #error Unsupported
+  #endif
+}
+
 static inline unsigned long lock_acquire (uint64_t *lock, unsigned long threadnum) {
   uint32_t* const pnext = (uint32_t*)lock;
   uint32_t* const powner = &pnext[1];
 
   const uint32_t my_ticket = __atomic_fetch_add(pnext, (uint32_t)1, __ATOMIC_SEQ_CST);
 
-  uint32_t now_serving;
+  uint32_t now_serving = my_ticket;
   do {
+    while (now_serving++ != my_ticket) {
+      spin_pause();
+    }
+
     now_serving = __atomic_load_n(powner, __ATOMIC_SEQ_CST);
   } while(now_serving != my_ticket);
 
