abfd57b8ed555eb061434127927474f0b331496f
Haris Okanovic <harisokn@amazon.com>
2023-11-16 18:05:37 -0600

    back to lock var

--- benchmarks/lockhammer/tests/ticket_spinlock_xxx.h
+++ benchmarks/lockhammer/tests/ticket_spinlock_xxx.h
@@ -1,7 +1,7 @@
 // XXX
 
-uint64_t test_lock_xxx;
-uint64_t test_lock;
+//uint64_t test_lock_xxx;
+//uint64_t test_lock;
 
 static inline void spin_pause() {
   #if defined(__x86_64__)
@@ -14,8 +14,8 @@ static inline void spin_pause() {
 }
 
 static inline unsigned long lock_acquire (uint64_t *lock, unsigned long threadnum) {
-  uint32_t* const pnext = (uint32_t*)(&test_lock);
-  uint32_t* const powner = (uint32_t*)(&test_lock_xxx);
+  uint32_t* const pnext = (uint32_t*)lock;
+  uint32_t* const powner = &pnext[1];
 
   const uint32_t my_ticket = __atomic_fetch_add(pnext, (uint32_t)1, __ATOMIC_ACQ_REL);
 
@@ -32,40 +32,8 @@ static inline unsigned long lock_acquire (uint64_t *lock, unsigned long threadnu
 }
 
 static inline void lock_release (uint64_t *lock, unsigned long threadnum) {
-  uint32_t* const pnext = (uint32_t*)(&test_lock);
-  uint32_t* const powner = (uint32_t*)(&test_lock_xxx);
+  uint32_t* const pnext = (uint32_t*)lock;
+  uint32_t* const powner = &pnext[1];
 
   __atomic_fetch_add(powner, (uint32_t)1, __ATOMIC_ACQ_REL);
 }
-
-
-
-/* with uint16_t + __ATOMIC_SEQ_CST
-       │     lock_acquire():                                                                                                                                                                                                                                                     ▒
-  0.00 │       ldaddalh w21, w2, [x19]                                                                                                                                                                                                                                           ▒
-  0.00 │       and      w2, w2, #0xffff                                                                                                                                                                                                                                          ▒
-       │                                                                                                                                                                                                                                                                         ▒
-       │     uint16_t now_serving;                                                                                                                                                                                                                                               ▒
-       │     do {                                                                                                                                                                                                                                                                ▒
-       │     now_serving = __atomic_load_n(powner, __ATOMIC_SEQ_CST);                                                                                                                                                                                                            ▒
- 66.25 │1a4:   ldarh    w1, [x27]                                                                                                                                                                                                                                                ▒
-       │     } while(now_serving != my_ticket);                                                                                                                                                                                                                                  ▒
-  0.08 │       cmp      w2, w1, uxth                                                                                                                                                                                                                                             ▒
- 33.37 │     ↑ b.ne     1a4                                                                                   
- */
-
-/* with uint16_t+ __ATOMIC_RELAXED
-       │     lock_acquire():                                                                                                                                                                                                                                                     ▒
-  0.00 │       ldaddh w21, w2, [x19]                                                                                                                                                                                                                                             ▒
-  0.00 │       and    w2, w2, #0xffff                                                                                                                                                                                                                                            ▒
-       │                                                                                                                                                                                                                                                                         ▒
-       │     uint16_t now_serving;                                                                                                                                                                                                                                               ▒
-       │     do {                                                                                                                                                                                                                                                                ▒
-       │     now_serving = __atomic_load_n(powner, __ATOMIC_RELAXED);                                                                                                                                                                                                            ▒
- 66.79 │1a4:   ldrh   w1, [x27]                                                                                                                                                                                                                                                  ▒
-       │     } while(now_serving != my_ticket);                                                                                                                                                                                                                                  ▒
-  0.08 │       cmp    w2, w1, uxth                                                                                                                                                                                                                                               ◆
- 32.77 │     ↑ b.ne   1a4
-*/
-
-
